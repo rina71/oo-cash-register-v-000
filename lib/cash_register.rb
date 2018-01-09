67acba2dacd173b97dc272ec8e017e2a06e50acb
Yurina Shin <yurinas@gmail.com>
2018-01-09 23:08:11 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
 
   def add_item(title,price,quantity = 1)
-    @items << title * quantity.times
+    @items.insert(quantity, title) 
     @total += price * quantity
 
   end
