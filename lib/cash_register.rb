1f42d050586641bfff974542dadd3d1ce1366297
Yurina Shin <yurinas@gmail.com>
2018-01-09 22:30:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total 
+  attr_accessor :total
 
   def initialize(discount = 0)
     @total = 0
@@ -11,6 +11,6 @@ class CashRegister
   def add_item(title,price)
     @items << title
     @total += price 
-    
+
   end
 end
