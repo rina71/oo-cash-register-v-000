89a7eb9e075966ad5e4edf893a93fe4b4439244c
Yurina Shin <yurinas@gmail.com>
2018-01-09 22:30:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total 
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title,price)
+    @items << title
+    @total += price 
+    
+  end
+end
