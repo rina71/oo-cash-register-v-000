17d369d4b12744eecec4d3816ff6f7c4f7e78bb4
Yurina Shin <yurinas@gmail.com>
2018-01-09 23:30:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,7 @@ class CashRegister
   def apply_discount
     if discount > 0
       @total = @total - self.discount
+      binding.pry
       puts "After the discount, the total comes to #{@total}."
     else
       "There is no discount to apply."
